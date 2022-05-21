b12498ff046568e69521ee585c1643fb64f86e63
doanh11 <trancongdoanh150896@gmail.com>
2022-05-21 14:15:27 +0700

    fix

--- baitap5.js
+++ baitap5.js
@@ -1,18 +1,37 @@
-function main(){
-    var X = +document.getElementById("SNTX").value ;
-   for (var i = 1 ; i <= X ; i++){
-        var kiemTra = 0;
-       for (j = 1 ; j <= i ;j++){
-           if (i % j === 0){
-            kiemTra++ ;
-           }
-       }
-       if (kiemTra === 2){
-           document.getElementById("ketQua").innerHTML+= ` ${i}`
-       }
-   }
 
-  
+function isPrime(n){
+    //flag = 0 => không phải số nguyên tố
+    //flag = 1 => số nguyên tố
+    
+    var num = 1;
+
+    if (n <2) return flag = 0; /*Số nhỏ hơn 2 không phải số nguyên tố => trả về 0*/
+    
+    /*Sử dụng vòng lặp while để kiểm tra có tồn tại ước số nào khác không*/
+    var i = 2;
+    while(i <n){
+        if( n%i==0 ) {
+            num = 0;
+            break; /*Chỉ cần tìm thấy 1 ước số là đủ và thoát vòng lặp*/
+        }
+        i++;
+    }
+
+    return num;
 }
 
+function kiemtra(){
+    var  sox = +document.getElementById('SNTX').value;
+    var i = 0;
+    var check = 0 ;
+    var ketQua ="";
+       while ( i < sox){
+        check = isPrime(i);
+        if( check === 1 ){
+            ketQua += i + " "; 
+        }
+        i++;
+    }
+    document.getElementById("ketqua").innerHTML = `${ketQua}` 
+}
 
